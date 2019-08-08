ba4df46e96f367e5e33c7879106c9f46b537edad
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-08-07 17:30:50 -0700

    Commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const Contact = () => <h1>Contact</h1>;
+const Contact = () => {
+
+return (
+
+    <div>
+
+        <h1>Contact</h1>
+    
+    </div>
+)
+
+}
 
 export default Contact;
