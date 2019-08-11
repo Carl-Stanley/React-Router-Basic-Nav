23c3af0fd71c3d37d7ec70f0c50f0dd13a387916
Carl-Stanley <cds.beml@gmail.com>
2019-08-11 08:47:00 -0700

    Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
+
     <Router>
-    <App />
+
+        <App />
+
     </Router>
+
+
     , document.getElementById('root')
 );
